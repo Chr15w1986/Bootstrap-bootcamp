900edad3cfae66345c2faca19088bde96d474b28
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-03-12 16:12:38 +0000

    d error response functionwith error message

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -8,4 +8,24 @@ function fetchGitHubInformation(event) {
     $("#gh-user-data").html(`<div id="loader">
         <img src="assets/css/loader.gif" alt="loading..."/>
         </div>`);
+
+    $.when(
+        $.getJSON(`http://api.github.com/users/${username}`)
+    ).then(
+        function (response) {
+            const userData = response;
+            $("gh-user-data").html(userInformationHTML(userData));
+        },
+        function (errorResponse) {
+            if(errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>Sorry, No info found for user ${username}</h2>`
+                );
+            } else {
+                console.log(errorResponse);
+                $("gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
+                );
+            }
+        });
 }
