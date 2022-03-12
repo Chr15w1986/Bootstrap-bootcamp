699f545a88e211402f11876dfb9a8313981d01b2
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-03-12 15:55:12 +0000

    add username data and loader.gif for github API

---
+++ assets/js/github-information.js
@@ -0,0 +1,11 @@
+function fetchGitHubInformation(event) {
+    const username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a Github username.</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(`<div id="loader">
+        <img src="assets/css/loader.gif" alt="loading..."/>
+        </div>`);
+}
