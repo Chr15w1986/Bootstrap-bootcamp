d258952cc45e6e473ff857529fb7f3588fa74967
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-03-12 17:59:29 +0000

    add fetch github information for user profiles

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,7 +1,25 @@
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+
 function fetchGitHubInformation(event) {
     const username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(`<h2>Please enter a Github username.</h2>`);
+        $("#gh-user-data").html(`<h3>Please enter a Github username.</h3>`);
         return;
     }
 
@@ -10,20 +28,20 @@ function fetchGitHubInformation(event) {
         </div>`);
 
     $.when(
-        $.getJSON(`http://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
         function (response) {
             const userData = response;
-            $("gh-user-data").html(userInformationHTML(userData));
+            $("#gh-user-data").html(userInformationHTML(userData));
         },
         function (errorResponse) {
-            if(errorResponse.status === 404) {
+            if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>Sorry, No info found for user ${username}</h2>`
                 );
             } else {
                 console.log(errorResponse);
-                $("gh-user-data").html(
+                $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
                 );
             }
